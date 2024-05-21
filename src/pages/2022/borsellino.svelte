595334d061dc159af827ef1756846911a70a792b
lorenzorizzolo <lolloriz36@gmail.com>
2024-05-21 09:55:58 +0200

    Aggiunto

--- src/pages/2022/borsellino.svelte
+++ src/pages/2022/borsellino.svelte
@@ -15,6 +15,7 @@
     <Block>
       <BlockTitle>Il libro <i>"La casa di paolo"</i></BlockTitle>
       Questo libro, che abbiamo letto per educazione civica è un gran esempio di etica sociale. Parla di questo magistrato, ormai conosciuto in tuto il mondo, Paolo Borsellino. Per la precisazione parla di un ragazzo di 15 anni che si ritrova in lui e nelle sue idee. Si racconta che Borsellino ha salvato la vita di questo ragazzo, non in modo oggettivo ma gliel'ha salvata dandogli delle lezioni di vita attraverso le sue parole ed i suoi fatti. Per l'appunto si parla che all'inizio questo ragazzo era molto agitato e non seguiva le regole, ovvero ciò che tiene in ordine la società e che impedisce il caos. Poi scopre questo personaggio e senza accoAll'inizio della seconda superiore la professoressa decide di affrontare questo argomento con la classe, esso comincia subito ad interessarsi ed a porsi molte domanda su di lui. Alla fine dell'anno la sua classe affronta un gita in merito dell'argomento andando fino a Palermo a vedere la vecchia casa di Paolo, ormai divenuta una casa per i bambini più disagiati della città.
+      Morto il 19 luglio 1992, nella strage di Via D'Amelio a seguito di un esplosione di una vettura parcheggiata nella via.
     </Block>
   
   </Page>
